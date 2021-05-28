d8184970dee01d11610d9fea18a042f45ba0155b
Michael Eguina <adriverforlife@gmail.com>
2021-05-28 06:55:54 -0700

    Add Events to Front Page

---
+++ src/Components/EventFrontPage.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import { Typography, Button, ButtonGroup, Container, makeStyles } from '@material-ui/core'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link'
+import Moment from 'react-moment';
+import axios from 'axios';
+
+const useStyles = makeStyles({})
+
+
+export default function EventList(props) {
+  const classes = useStyles()
+
+  const [events, getEvents] = useState([]);
+
+  useEffect(() => {
+    getAllEvents();
+  }, []);
+
+  function getAllEvents() {
+    axios.get(`https://api.motorsportreg.com/rest/calendars/organization/F19F66B9-D516-5FEE-B4ABB9A12E335B59.json`)
+      .then(res => {
+        const allEvents = res.data.response.events;
+        getEvents(allEvents)
+      })
+  }
+
+
+  return (
+
+    <TableContainer component={Paper}>
+      <Typography variant="h5" component="h2">
+        Upcoming Events</Typography>
+        <Typography variant="body2" component="p">We host a small autocross series of our own. Our upcoming events are listed below. Go to the <Link href='/autocross'>Autocross Challenge</Link> Page for more details!</Typography>
+      <Table className={classes.table} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell align="right">Event</TableCell>
+            <TableCell align="right">Venue</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {events.map((event) => (
+            <TableRow key={event.name}>
+              <TableCell component="th" scope="event">
+                <Link href={event.detailuri}> {event.name}</Link>
+              </TableCell>
+              <TableCell align="right"><Moment format="MM/DD/YYYY">{event.start}</Moment></TableCell>
+              <TableCell align="right">{event.venue.name}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+  )
+
+}
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,6 +4,7 @@ import { Home, ImportContacts, Send } from '@material-ui/icons'
 import TextCard from '../Components/TextCard'
 import TitlePic from '../pictures/996gt3.jpeg'
 import Subiefest from '../pictures/subiefest.jpg'
+import EventFrontPage from '../Components/EventFrontPage'
 
 const useStyles = makeStyles({
 
@@ -25,7 +26,7 @@ const classes = useStyles()
         // pic={Subiefest}
         body='We have helped with individual car testing all the way to staffing large autocross event as Subiefest and the Porsche Owners Club Autocross Series! If you just need a couple cones all the way to a full autocross series we can handle it all!Clients have included Speed Ventures, 86Fest, Subiefest, Triumph Club, SCCA Rallycross, various BMW Club Chapters, and more! We can accommodate events of almost any size, as we have several sets of timing equipment, and WAY too many cones!'
         />
-
+      <EventFrontPage/>
     </Container>
   )
 }
